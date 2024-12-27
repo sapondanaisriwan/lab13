7c137181f800ac6a1a38467c669032571be4013e
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2024-12-27 16:15:32 +0700

    .

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,13 +3,14 @@
 #include <cmath>
 using namespace std;
 
-void stat(const double[],int,double[]);
+void stat(const double[], int, double[]);
 
-int main(){
-    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
-    int N = sizeof(A)/sizeof(A[0]);
-    double B[6];
-    stat(A,N,B);
+int main()
+{
+    double A[] = {1.2, 3.5, 6.9, 7.8, 12.5, 0.5};
+    int N = sizeof(A) / sizeof(A[0]);
+    double B[6] = {};
+    stat(A, N, B);
     cout << fixed << setprecision(2);
     cout << "Arithmetic Mean = " << B[0] << endl;
     cout << "Standard Deviation = " << B[1] << endl;
@@ -18,4 +19,35 @@ int main(){
     cout << "Max = " << B[4] << endl;
     cout << "Min = " << B[5];
     return 0;
+}
+
+void stat(const double source[], int length, double dest[])
+{
+    double mean = 0, sum = 0, sum2 = 0, sd = 0, geo = 1;
+    double min = source[0], max = source[0];
+    for (int i = 0; i < length; i++)
+    {
+        sum += source[i];
+        sum2 += 1.0 / source[i];
+        geo *= source[i];
+        if (min < source[i])
+            min = source[i];
+        if (max > source[i])
+            max = source[i];
+    }
+    mean = sum / length;
+    geo = pow(geo, (1.0 / length));
+    double temp = 0;
+    for (int i = 0; i < length; i++)
+    {
+        temp += pow(source[i] - mean, 2);
+    }
+    sd = pow(temp / length, 0.5);
+
+    dest[0] = mean;
+    dest[1] = sd;
+    dest[2] = geo;
+    dest[3] = length / sum2;
+    dest[4] = min;
+    dest[5] = max;
 }
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -5,19 +5,60 @@ using namespace std;
 const int N = 30;
 const int M = 70;
 
-void updateImage(bool [][M],int,int,int);
+void updateImage(bool[][M], int, int, int);
 
-void showImage(const bool [][M]);
+void showImage(const bool[][M]);
 
 int main()
 {
     bool image[N][M] = {};
-    int s,x,y;
-    do{
+    int s, x, y;
+    do
+    {
         showImage(image);
         cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
-        updateImage(image,s,x,y);
-    }while(s != 0 || x != 0 || y != 0);
+        updateImage(image, s, x, y);
+    } while (s != 0 || x != 0 || y != 0);
     return 0;
+}
+
+void showImage(const bool pos[][M])
+{
+    cout << "------------------------------------------------------------------------\n";
+    for (int row = 0; row < 30; row++)
+    {
+        cout << "|";
+        for (int col = 0; col < 70; col++)
+        {
+            if (pos[row][col] == 0)
+            {
+                cout << " ";
+            }
+            else if (pos[row][col] == 1)
+            {
+                cout << "*";
+            }
+            if (col == 69)
+            {
+                cout << "|\n";
+            }
+        }
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
+
+void updateImage(bool pos[][M], int size, int row, int column)
+{
+    for (int i = 0; i < 30; i++)
+    {
+        for (int j = 0; j < 70; j++)
+        {
+            double formular = sqrt(pow((i - row), 2) + pow((j - column), 2));
+            if (formular <= size - 1)
+            {
+                pos[i][j] = 1;
+            }
+        }
+    }
 }
